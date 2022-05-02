27c70ab3ef5c61f5476759a50c702edfaba927b9
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2022-05-02 10:57:46 -0600

    init commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Swap Two Numbers in Python | Three Methods
+Learn how to swap two numbers in python using three different methods. This is a Python for beginners tutorial.
+Watch the tutorial on YouTube at https://youtu.be/u31ylzeN0DY
---
+++ swap_numbers.py
@@ -0,0 +1,30 @@
+### WATCH THE VIDEO TUTORIAL ON MY YOUTUBE CHANNEL
+### https://youtu.be/u31ylzeN0DY
+
+a = 5
+b = 4
+
+print('A: ', a)
+print('B: ', b)
+
+# METHOD ONE
+c = a
+a = b
+b = c
+
+# METHOD TWO.A
+# a = a + b
+# b = a - b
+# a = a - b
+
+# METHOD TWO.B
+# a = a * b
+# b = a / b
+# a = a / b
+
+# METHOD THREE
+# a, b = b, a
+
+print('-----')
+print('A: ', a)
+print('B: ', b)
